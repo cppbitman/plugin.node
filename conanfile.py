ab3f148608f541db5e1bdee40e9b7982b6303c45
cppbitman <cppbitman@users.noreply.github.com>
2018-06-14 17:16:12 +0800

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -17,7 +17,8 @@ def _UNUSED(*param):
 class NodePlugin(ConanFile):
     name = "plugin.node"
     version = "0.5.3-dev"
-    url = "https://github.com/Mingyiz/plugin.node"
+    #url = "https://github.com/Mingyiz/plugin.node"
+    url = "https://github.com/cppbitman/plugin.node"
     homepage = url
     description = "Node.js addon for c plugin dynamic."
     license = "Apache-2.0"
